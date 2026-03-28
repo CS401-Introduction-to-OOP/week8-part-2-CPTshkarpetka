ce404d02f2ac629c03efabe42a95294c28f2ba10
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 16:23:20 +0200

    Created DataItem class

--- Week8/PoinRef.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Week8;
-
-public class PoinRef
-{
-    public int X { get; set;}
-    public int Y { get; set;}
-    public void Print() => Console.WriteLine($"({X}, {Y})");
-}
--- Week8/Point.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Week8;
-
-public struct Point
-{
-    public int X { get; set;}
-    public int Y { get; set;}
-    public void Print() => Console.WriteLine($"({X}, {Y})");
-}
