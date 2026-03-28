730590bc5da523f259c8d015482da28fb0d741c3
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 16:17:53 +0200

    Completed stage2

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a; //boxing
+        int c = (int)b; //unboxing
+        Console.WriteLine($"a: {a}, b:{b}, c:{c}");
+    }
+}
