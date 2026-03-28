3e50abc843f0da8cc200c0d3815af9bbc74d6f38
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 16:17:44 +0200

    Completed stage 1

---
+++ Week8/PoinRef.cs
@@ -0,0 +1,8 @@
+﻿namespace Week8;
+
+public class PoinRef
+{
+    public int X { get; set;}
+    public int Y { get; set;}
+    public void Print() => Console.WriteLine($"({X}, {Y})");
+}
--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,14 +1,1 @@
-﻿using Week8;
-
-var file = new FileResource("report.txt");
-var network = new NetworkResource("api.company.local");
-var manager = new ResourceManager<Resource>();
-manager.Add(file);
-manager.Add(network);
-manager.OpenAll();
-using (var tempResource = new FileResource("temporary_data.tmp"))
-{
-    tempResource.Open();
-}
-manager.CloseAll();
-Console.WriteLine("Done.");
+﻿Console.WriteLine("Hello, World!");
