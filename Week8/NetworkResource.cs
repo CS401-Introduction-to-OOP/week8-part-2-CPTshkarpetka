a1024ba49a25bcaaa3566815c24eb746c3b93275
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 15:52:11 +0200

    Created NetworkResource class

---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    
+    public override void Open()
+    {
+        if (isOpen) return;
+        isOpen = true;
+        Console.WriteLine($"{Name} is opened");
+    }
+    
+    public override void Close()
+    {
+        if (!isOpen) return;
+        isOpen = false;
+        Console.WriteLine($"{Name} is closed");
+    }
+    public void Dispose()
+    {
+        Console.WriteLine($"{Name} is disposed");
+        Close();
+    }
+}
