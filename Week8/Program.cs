841fefb3e7a33a468c34770183e0e51311924c9c
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 16:00:12 +0200

    created Program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,14 @@
-﻿
-class Program
+﻿using Week8;
+
+var file = new FileResource("report.txt");
+var network = new NetworkResource("api.company.local");
+var manager = new ResourceManager<Resource>();
+manager.Add(file);
+manager.Add(network);
+manager.OpenAll();
+using (var tempResource = new FileResource("temporary_data.tmp"))
 {
-    public static void Main()
-    {
-        
-    }
-    
+    tempResource.Open();
 }
+manager.CloseAll();
+Console.WriteLine("Done.");
