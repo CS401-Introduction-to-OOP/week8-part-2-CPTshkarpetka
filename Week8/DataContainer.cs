86078ef566958eed2149d75e7f9914d5a51fd5a8
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 16:24:44 +0200

    Created DataContainer class

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,18 @@
+﻿namespace Week8;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item) => _items.Add(item);
+
+    public long GetTotalSize()
+    {
+        long sum = 0;
+        foreach (T item in _items)
+        {
+            sum += item.GetSize();
+        }
+
+        return sum;
+    }
+}
